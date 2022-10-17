8e5ddc38f92f8d5966b11d52c5b0ad0ea8ec6488
dizzy1021 <dimasadiprananda@gmail.com>
2022-10-17 15:00:18 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Interview PT. Impack Pratama Group
+
+`` Objective ``
+
+- REST API Product
+  - [] POST Product
+  - [] PUT Product
+  - [] DELETE Product
+  - [] GET Product with filter and pagination
+
+`` Structure ``
+
+- Product Table
+  - Code
+  - Name
+  - Description
+  - Price
+  - UOM (Unit of Measurement) = SHEET, ROLL, PCS
+
+`` TODO ``
+
+- [] Environtment Variable
+- [] Use Basic Auth to secure API
+- [] Rate Limiter
+- [] Mysql Adapter
+- [] Request Validation
+- [] Response JSON API
+- [] API Versioning
+- [] Unit Test
+- [] API Docs
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	router  := gin.Default()
+
+	port := loadEnv("PORT")
+	readTimeout, err := strconv.Atoi(loadEnv("READ_TIMEOUT"))
+	if err != nil {
+		log.Fatalf("Error load read timeout env")
+	}
+
+	writeTimeout, err := strconv.Atoi(loadEnv("WRITE_TIMEOUT"))
+	if err != nil {
+		log.Fatalf("Error load write timeout env")
+	}
+
+	appMode := loadEnv("APP_MODE")
+	
+	server := &http.Server{
+		Addr		: ":"+port,
+		Handler		: router,
+		ReadTimeout	: time.Duration(readTimeout) * time.Second,
+		WriteTimeout: time.Duration(writeTimeout) * time.Second,		
+	}
+
+	gin.SetMode(appMode)
+
+	server.ListenAndServe()
+}
+
+func loadEnv(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
